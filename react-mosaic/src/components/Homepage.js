f76c89f31f2119a68ba69fb4167bec6beca7bb69
AaryadevChandra <aaryadevc@gmail.com>
2022-05-24 03:33:22 +0530

    changes

--- react-mosaic/src/components/Homepage.js
+++ react-mosaic/src/components/Homepage.js
@@ -30,45 +30,204 @@ const HomePageComponent = () =>{
 
         {/* our platforms starts */}
 
-        <div className='container-fluid' style={{border: '1px solid blue'}}>
-            <div className='row text-center' style={{border: '1px solid green'}}>
+        <div className='container-fluid' style={{border: '1px solid blue', marginTop: '5vh'}}>
+            <div className='row text-center' style={{border: '1px solid green', marginBottom: '4vh'}}>
                 <div className='col' style={{fontSize: '5vh'}}>
                     Our Platforms
                 </div>
             </div>
 
-            <div className='row text-center' style={{border: '1px solid green'}}>
-                <div className='col card shadow' style={{marginLeft: '10vw', marginRight: '0.5vw', border: '1px solid yellow', maxWidth: '400px'}}>
+            <div className='row text-center justify-content-center' style={{border: '1px solid green'}}>
+                <div className='col card shadow' style={{marginLeft: '10vw', marginRight: '0.5vw', border: '1px solid yellow', maxWidth: '400px', fontSize: '2.5vh'}}>
                         Digital Health Coach for Men
                         <br/>
                         <br/>
                         <p className='light-font'>Hair. Skin. Weight. Sleep.</p>
                         <p className='light-font'>If it matters to you, it matters to us</p>
                         
-                        <div className='row'>
-                        <p>
-                            <a className='navbar-links manmatters-font-blue' href="manmatters.com">manmatters.com</a>
-                        </p>
-                        </div>
+                        <a className='navbar-links manmatters-font-blue' href="manmatters.com">manmatters.com</a>
                 </div>
 
 
 
-                <div className='col card shadow' style={{marginRight: '10vw', marginLeft: '0.5vw', border: '1px solid yellow', maxWidth: '400px'}}>
+                <div className='col card shadow' style={{marginRight: '10vw', marginLeft: '0.5vw', border: '1px solid yellow', maxWidth: '400px', fontSize: '2.5vh'}}>
                     Digital Health Coach for Women
                     <br/>
                     <br/>
                     <p className='light-font'>Bodywise is a wellness revolution designed to help you reclaim control over your health.</p>
-                    <div className='row'>
-                        <p>
-                            <a className='navbar-links bodywise-font-yellow' href="bebodywise.com">bebodywise.com</a>
-                        </p>
-                    </div>
+                    <a className='navbar-links bodywise-font-yellow' href="bebodywise.com">bebodywise.com</a>
                 
                 
                 </div>
             </div>
         </div>
+
+        {/* our platforms ends */}
+
+        <div className='container-fluid' style={{border: '1px solid blue', marginTop: '5vh'}}>
+            <div className='row text-center' style={{border: '1px solid green', marginBottom: '4vh'}}>
+                <div className='col' style={{fontSize: '5vh'}}>
+                    Our Values
+                </div>
+            </div>
+
+            <div className='row text-center justify-content-center' style={{border: '1px solid green'}}>
+                <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px', marginRight: '1vw'}}>
+                Authentic with all 
+                <br/>
+                stakeholders
+                </div>
+
+                <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px', marginRight: '1vw'}}>
+                Bias for 
+                <br/>
+                action
+                </div>
+
+                <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px'}}>
+                Passionate to do what 
+                <br/>
+                is best for the users
+                </div>  
+            </div>
+
+            <div className='row text-center justify-content-center' style={{border: '1px solid green', marginTop: '2vh'}}>
+            <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px', marginRight: '1vw'}}>
+                Authentic with all 
+                <br/>
+                stakeholders
+                </div>
+
+                <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px', marginRight: '1vw'}}>
+                Bias for 
+                <br/>
+                action
+                </div>
+
+                <div className='col-2 card shadow my-auto' style={{border: '1px solid yellow', padding : '25px'}}>
+                Passionate to do what 
+                <br/>
+                is best for the users
+                </div>
+            </div>
+
+        </div>
+        
+        {/* milestones start */}
+
+        <div className='container-fluid' style={{marginTop: '7vh'}}>
+            <div className='row text-center' style={{fontSize: '5vh'}}>
+                <div className='col'>
+                    Milestones
+                </div>
+            </div>
+            
+            <div className='row text-center'>
+                <div className='col'>
+                    <img src='https://uploads-ssl.webflow.com/625c1ce330600d5866988c42/625c6a4896cc1171bd88719f_Group%201773-p-1080.png' />
+                </div>
+            </div>
+        </div>
+
+        {/* milestones end */}
+
+        <div className='container-fluid' style={{marginTop: '5vh'}}>
+            <div className='row text-center justify-content-center' style={{fontSize: '5vh'}}>
+                <div className='col-7'>
+                    Careers
+                    <p className='light-font' style={{fontSize: '2.5vh'}}>
+                    We're always on the lookout for people who share our passion to create.
+We have open positions within brand design, tech and product design. If you feel the compelling need to build with independence and ownership, we'd love to hear from you.
+                    </p>
+                </div>
+            </div>
+
+            <div className='row text-center' style={{marginTop: '5vh'}}>
+                <div className='col'>
+                    <button className='btn btn-lg btn-block apply-btn input-shadow' style={{padding:'25px'}}>
+                        Apply for open roles
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        {/* Careers end */}
+
+        <div className='container-fluid' style={{marginTop: '5vh', border: '1px solid blue'}}>
+            <div className='row justify-content-center' style={{border: '1px solid green'}}>
+                <div className='col-7 card input-shadow ' style={{fontSize: '5vh'}}>
+                    <p className='text-center'>Say hello!</p>
+                    <p className='light-font text-center' style={{fontSize: '2.5vh'}}>Write to us & we will get back to you.</p>
+
+
+                    <div className='row'>
+                        <div className='col light-font m-2' style={{fontSize: '2vh'}}>
+                            NAME
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            EMAIL ADDRESS
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row'>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            PHONE
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row'>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            YOUR MESSAGE
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row'>
+                        <div className='col'>
+                            
+                        </div>
+                    </div>
+
+               
+                </div>
+            </div>
+        
+
+            
+
+        
+        </div>
+
+        
 
         </>
     )
