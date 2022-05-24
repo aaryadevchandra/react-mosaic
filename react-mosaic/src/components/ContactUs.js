ccf4fa30805b17dc1c58971331f42eaded8f15e4
AaryadevChandra <aaryadevc@gmail.com>
2022-05-24 09:53:41 +0530

    initial commit

---
+++ react-mosaic/src/components/ContactUs.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+const ContactUsComponent = ()=> {
+
+    return (
+        <>
+            <div className='container-fluid' style={{marginTop: '5vh', border: '1px solid blue'}}>
+            <div className='row justify-content-center' style={{border: '1px solid green'}}>
+                <div className='col-7 card input-shadow' style={{fontSize: '5vh'}}>
+                    <p className='text-center'>Say hello!</p>
+                    <p className='light-font text-center' style={{fontSize: '2.5vh'}}>Write to us & we will get back to you.</p>
+
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div className='col light-font' style={{fontSize: '2vh'}}>
+                            NAME
+                        </div>
+                    </div>
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            EMAIL ADDRESS
+                        </div>
+                    </div>
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            PHONE
+                        </div>
+                    </div>
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div className='col light-font' style={{fontSize: '2vh', marginTop: '2.5vh'}}>
+                            YOUR MESSAGE
+                        </div>
+                    </div>
+
+                    <div className='row' style={{marginLeft: '1vw', marginRight: '1vw'}}>
+                        <div  className='form-group'>
+                            <input className='form-control shadow-none'></input>
+                        </div>
+                    </div>
+
+
+                    <div className='row text-center' style={{marginTop: '5vh'}}>
+                        <div className='col'>
+                        <button className='btn btn-lg btn-block apply-btn input-shadow' style={{paddingRight:'10vw', paddingLeft: '10vw'}}>
+                            Submit
+                        </button>
+                        </div>
+                    </div>
+
+               
+                </div>
+            </div>
+        </div>
+
+        </>
+    )
+
+}
+
+export default ContactUsComponent;
