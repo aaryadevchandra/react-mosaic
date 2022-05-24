b06887742335df08b9ea79c4d0a2c50348c4355e
AaryadevChandra <aaryadevc@gmail.com>
2022-05-24 09:55:48 +0530

    added fellowship path on browser router

--- react-mosaic/src/index.js
+++ react-mosaic/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import HomePageComponent from './components/Homepage';
 import NavbarComponent from './components/Navbar';
+import FellowshipComponent from './components/Fellowship';
 
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<HomePageComponent />}/>
+      <Route path='/fellowship' element={<FellowshipComponent/>} />
     </Routes>
   </BrowserRouter>
 , document.getElementById("root"));
